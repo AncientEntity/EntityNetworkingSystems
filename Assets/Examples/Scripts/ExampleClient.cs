04d882b6b6b1b701a5c14a5da39edb89fd129f9c
AncientEntity <rogoclub@outlook.com>
2020-06-03 23:35:18 -0700

    NetClient/NetServer basics

---
+++ Assets/Examples/Scripts/ExampleClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleClient : MonoBehaviour
+{
+    public NetClient netClient;
+
+    public void ConnectToServer()
+    {
+        netClient = new NetClient();
+        netClient.Initialize();
+        netClient.ConnectToServer();
+    }
+}
---
+++ Assets/Examples/Scripts/ExampleHost.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleHost : MonoBehaviour
+{
+    public NetServer netServer;
+
+    public void StartServer()
+    {
+        netServer = new NetServer();
+        netServer.Initialize();
+    }
+
+}
