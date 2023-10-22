deec22837d57f876d08e692b8604e8021c79dec4
Ryan <ryanhasman10@hotmail.com>
2023-10-22 12:34:21 -0700

    Modified UPnP to fix a few issues

--- Assets/EntityNetworkingSystems/Scripts/NetBackbone/UPnP.cs
+++ Assets/EntityNetworkingSystems/Scripts/NetBackbone/UPnP.cs
@@ -1,6 +1,7 @@
 ﻿using Mono.Nat;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace EntityNetworkingSystems.Nat
@@ -11,10 +12,14 @@ namespace EntityNetworkingSystems.Nat
         //https://github.com/alanmcgovern/Mono.Nat/blob/master/Mono.Nat.Console/Main.cs
 
         private static INatDevice device;
-        private static Mapping currentMapping;
+        private static List<Mapping> currentMappings = new List<Mapping>();
 
         public static void Init()
         {
+            if(device != null)
+            {
+                return; //Already initialized.
+            }
             NatUtility.DeviceFound += DeviceFound;
             NatUtility.StartDiscovery();
             Debug.Log("ENS Nat Initialized");
@@ -22,13 +27,36 @@ namespace EntityNetworkingSystems.Nat
 
         public static async void Shutdown()
         {
-            if (currentMapping != null && device != null)
+            if (currentMappings != null && device != null)
             {
-                await device.DeletePortMapAsync(currentMapping);
+                foreach(Mapping m in currentMappings)
+                {
+                    await device.DeletePortMapAsync(m);
+                }
             }
             Debug.Log("Port Mappings Cleaned Up");
         }
         
+        public static async Task<Mapping> CreateNewMapping(Mapping portMapping)
+        {
+            try
+            {
+                if (device != null)
+                {
+                    //Create Mapping
+                    //Debug.Log("Creating uPnP Mapping: protocol=" + portMapping.Protocol + "ports=" + portMapping.PublicPort);
+                    Mapping createdMap = await device.CreatePortMapAsync(portMapping);
+                    Debug.Log("Created Map: protocol=" + createdMap.Protocol + "ports=" + createdMap.PublicPort);
+                    currentMappings.Add(createdMap);
+                    return createdMap;
+                }
+            } catch (System.Exception e)
+            {
+                Debug.LogWarning("Error Caught: "+e);
+            }
+            return null;
+        }
+
 
         private static async void DeviceFound(object sender, DeviceEventArgs args)
         {
@@ -40,12 +68,6 @@ namespace EntityNetworkingSystems.Nat
                 //Debug.Log("Device Name: " + device.GetType().Name);
                 //Debug.Log("Device External IP: " + (await device.GetExternalIPAsync()).ToString());
 
-                //Create Mapping
-                Mapping portMapping = new Mapping(Protocol.Tcp, NetServer.serverInstance.hostPort, NetServer.serverInstance.hostPort);
-                //Debug.Log("Creating uPnP Mapping: protocol=" + portMapping.Protocol + "ports=" + portMapping.PublicPort);
-                Mapping createdMap = await device.CreatePortMapAsync(portMapping);
-                Debug.Log("Created Map: protocol=" + createdMap.Protocol + "ports=" + createdMap.PublicPort);
-                currentMapping = createdMap;
 
                 //try
                 //{
